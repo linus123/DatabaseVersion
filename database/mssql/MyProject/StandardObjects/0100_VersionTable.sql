a0cafbff34b112177b8783c92cdebd225f0cdfd9
Paul Herrera <pherrera0727@gmail.com>
2012-06-13 11:33:58 -0500

    Changed the format of the version table.

--- database/mssql/MyProject/StandardObjects/0100_VersionTable.sql
+++ database/mssql/MyProject/StandardObjects/0100_VersionTable.sql
@@ -1,11 +1,12 @@
 CREATE TABLE DatabaseVersion (
-	change_number BIGINT NOT NULL,
-	delta_set VARCHAR(10) NOT NULL,
-	start_dt DATETIME NOT NULL,
-	complete_dt DATETIME NULL,
-	applied_by VARCHAR(100) NOT NULL,
-	description VARCHAR(500) NOT NULL
+  ChangeNumber BIGINT NOT NULL,
+  Project VARCHAR(10) NOT NULL,
+  StartDate DATETIME NOT NULL,
+  CompletedDate DATETIME NULL,
+  AppliedBy VARCHAR(100) NOT NULL,
+  FileName VARCHAR(500) NOT NULL
 )
+GO
 
-ALTER TABLE DatabaseVersion
-	ADD CONSTRAINT PK_DatabaseVersion_ChangeNumberDeltaSet PRIMARY KEY (change_number, delta_set)
+ALTER TABLE DatabaseVersion ADD CONSTRAINT PkDatabaseVersion PRIMARY KEY (ChangeNumber, Project)
+GO
