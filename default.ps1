b914f108d17b835518737e499d780c17edef00dc
Paul Herrera <pherrera0727@gmail.com>
2013-05-21 12:59:30 -0500

    Added change bug "buckets"

--- default.ps1
+++ default.ps1
@@ -70,108 +70,134 @@ task RunTests {
 }
 
 task UpdateVersioningScripts -depends RunTests {
-		################################################################################################################################
-		#
-		# Script Name : SmoDb
-		# Version     : 1.0
-		# Author      : Vince Panuccio
-		# Purpose     :
-		#			  This script generates one SQL script per database object including Stored Procedures,Tables,Views, 
-		#			  User Defined Functions and User Defined Table Types. Useful for versionining a databsae in a CVS.
-		#
-		# Usage       : 
-		#			  Set variables at the top of the script then execute.
-		#
-		# Note        :
-		#			  Only tested on SQL Server 2008r2
-		#                 
-		################################################################################################################################
-		$server 			= $databaseServerName
-		$database 			= $databaseName
-		$output_path 		= $versioningScriptsFolder
-		 
-		$schema 			= "dbo"
-		$table_path 		= "$output_path\Table\"
-		$storedProcs_path 	= "$output_path\StoredProcedure\"
-		$views_path 		= "$output_path\View\"
-		$udfs_path 			= "$output_path\UserDefinedFunction\"
-		$textCatalog_path 	= "$output_path\FullTextCatalog\"
-		$udtts_path 		= "$output_path\UserDefinedTableTypes\"
-		 
-		[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | out-null
-		 
-		$srv 		= New-Object "Microsoft.SqlServer.Management.SMO.Server" $server
-		$db 		= New-Object ("Microsoft.SqlServer.Management.SMO.Database")
-		$tbl 		= New-Object ("Microsoft.SqlServer.Management.SMO.Table")
-		$scripter 	= New-Object ("Microsoft.SqlServer.Management.SMO.Scripter") ($server)
-		 
-		# Get the database and table objects
-		$db = $srv.Databases[$database]
-		 
-		$tbl		 	= $db.tables | Where-object { $_.schema -eq $schema  -and -not $_.IsSystemObject } 
-		$storedProcs	= $db.StoredProcedures | Where-object { $_.schema -eq $schema -and -not $_.IsSystemObject } 
-		$views 		 	= $db.Views | Where-object { $_.schema -eq $schema } 
-		$udfs		 	= $db.UserDefinedFunctions | Where-object { $_.schema -eq $schema -and -not $_.IsSystemObject } 
-		$catlog		 	= $db.FullTextCatalogs
-		$udtts		 	= $db.UserDefinedTableTypes | Where-object { $_.schema -eq $schema } 
-			
-		# Set scripter options to ensure only data is scripted
-		$scripter.Options.ScriptSchema 	= $true;
-		$scripter.Options.ScriptData 	= $false;
-		 
-		#Exclude GOs after every line
-		$scripter.Options.NoCommandTerminator 	= $false;
-		$scripter.Options.ToFileOnly 			= $true
-		$scripter.Options.AllowSystemObjects 	= $false
-		$scripter.Options.Permissions 			= $true
-		$scripter.Options.DriAllConstraints 	= $true
-		$scripter.Options.SchemaQualify 		= $true
-		$scripter.Options.AnsiFile 				= $true
-		 
-		$scripter.Options.SchemaQualifyForeignKeysReferences = $true
-		 
-		$scripter.Options.Indexes 				= $true
-		$scripter.Options.DriIndexes 			= $true
-		$scripter.Options.DriClustered 			= $true
-		$scripter.Options.DriNonClustered 		= $true
-		$scripter.Options.NonClusteredIndexes 	= $true
-		$scripter.Options.ClusteredIndexes 		= $true
-		$scripter.Options.FullTextIndexes 		= $true
-		 
-		$scripter.Options.EnforceScriptingOptions 	= $true
-		 
-		function CopyObjectsToFiles($objects, $outDir) {
-			
-			if (-not (Test-Path $outDir)) {
-				[System.IO.Directory]::CreateDirectory($outDir)
-			}
-			
-			foreach ($o in $objects) { 
-			
-				if ($o -ne $null) {
-					
-					$schemaPrefix = ""
-					
-					if ($o.Schema -ne $null -and $o.Schema -ne "") {
-						$schemaPrefix = $o.Schema + "."
-					}
-				
-					$scripter.Options.FileName = $outDir + $schemaPrefix + $o.Name + ".sql"
-					Write-Host "Writing " $scripter.Options.FileName
-					$scripter.EnumScript($o)
-				}
-			}
+	DeleteAndRecreateFolder "$baseDir\database\mssql\$projectName\VersioningScripts"
+
+	[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMO") | out-null;
+	[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.SMOExtended")| Out-Null;
+
+	$server = $databaseServerName
+	$dbname = $databaseName
+	$filepath = $versioningScriptsFolder
+
+	$SMOserver = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $server
+	$db = $SMOserver.databases[$dbname]
+
+	$Objects = $db.Tables
+	$Objects += $db.Views
+	$Objects += $db.StoredProcedures
+	$Objects += $db.UserDefinedFunctions
+
+	New-Item -Path ($filepath) -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'General') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Analytics') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Cash') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Portfolio') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Price') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Broker') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Benchmark') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Security') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Holding') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Trade') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'General') -ItemType directory -Force | Out-Null
+	New-Item -Path ($filepath + "\" + 'Unclassified') -ItemType directory -Force | Out-Null
+	
+	Write-Host "Iterating database object types"
+	
+	foreach ($ScriptThis in $Objects | where {!($_.IsSystemObject)}) 
+	{
+		$scriptr = new-object ('Microsoft.SqlServer.Management.Smo.Scripter') ($SMOserver)
+		$scriptr.Options.ScriptDrops = $true
+		$scriptr.Options.IncludeIfNotExists = $true
+		$scriptr.Options.DriPrimaryKey = $true
+		$scriptr.Options.DriChecks = $true
+		$scriptr.Options.DriClustered = $true
+		$scriptr.Options.DriDefaults = $true
+		$scriptr.Options.DriIndexes = $ture
+		$scriptr.Options.DriUniqueKeys = $true
+		$scriptr.Options.ExtendedProperties = $true
+		$scriptr.Options.Triggers = $true
+		$scriptr.Options.Indexes = $true
+		$scriptr.Options.ScriptBatchTerminator = $true
+		$scriptr.Options.EnforceScriptingOptions = $true
+		$scriptr.Options.DriChecks = $true
+		$scriptr.Options.AppendToFile = $False
+		$scriptr.Options.AllowSystemObjects = $False
+		$scriptr.Options.ClusteredIndexes = $True
+		$scriptr.Options.DriForeignKeys = $False
+		$scriptr.Options.ScriptDrops = $False
+		$scriptr.Options.IncludeHeaders = $True
+		$scriptr.Options.ToFileOnly = $True
+		$scriptr.Options.Indexes = $True
+		$scriptr.Options.WithDependencies = $False
+		$scriptr.Options.ChangeTracking = $True
+		$scriptr.Options.DriIndexes = $true
+		$scriptr.Options.SchemaQualify = $true
+		$scriptr.Options.IncludeDatabaseContext = $true
+		  
+		$description = "Unclassified"
+						
+		If($ScriptThis.ExtendedProperties -ne $null)
+		{
+			foreach($property in $ScriptThis.ExtendedProperties) 
+			{ 
+				if($property.Name -eq "Bucket") 
+				{ 
+					$description = $property.Value; 
+				} 
+			} 
 		}
-		 
-		# Output the scripts
-		CopyObjectsToFiles $tbl $table_path
-		CopyObjectsToFiles $storedProcs $storedProcs_path
-		CopyObjectsToFiles $views $views_path
-		CopyObjectsToFiles $catlog $textCatalog_path
-		CopyObjectsToFiles $udtts $udtts_path
-		CopyObjectsToFiles $udfs $udfs_path
-		 
-		Write-Host "Finished at" (Get-Date)
+		  
+		$ScriptFile = $ScriptThis -replace "\[|\]"
+		$scriptr.Options.FileName = $filepath + "\" + $($description) + "\" + $($ScriptFile) + "_" + $($ScriptThis.GetType().Name) + ".sql"
+
+		$scriptr.Script($ScriptThis)
+
+	}
+
+
+	$dbObjCollection = @();
+	foreach($tb in $db.Tables)
+	{
+		$dbObjCollection += $tb.ForeignKeys
+	}
+
+	foreach ($dbObj in $dbObjCollection) 
+	{   
+		$scriptr = new-object ('Microsoft.SqlServer.Management.Smo.Scripter') ($SMOserver)
+		$scriptr.Options.DriForeignKeys = $true
+		$scriptr.Options.SchemaQualifyForeignKeysReferences = $true
+		$scriptr.Options.ScriptBatchTerminator = $true
+		$scriptr.Options.ToFileOnly = $true
+		$scriptr.Options.SchemaQualify = $true
+		$scriptr.Options.ExtendedProperties = $true
+		$scriptr.Options.EnforceScriptingOptions = $true
+		$scriptr.Options.ChangeTracking = $True
+		$scriptr.Options.SchemaQualify = $true
+		$scriptr.Options.IncludeDatabaseContext = $true
+		
+		$smoObjects = @();
+		$smoObjects += $dbObj.Urn; 
+		if ($dbObj.IsSystemObject -eq $true) 
+		{ 
+			$sc = $scripter.EnumScript($smoObjects)
+		} 
+		
+		$description = "Unclassified"
+						
+		If($dbObj.ExtendedProperties -ne $null)
+		{
+			foreach($property in $ScriptThis.ExtendedProperties) 
+			{ 
+				if($property.Name -eq "Bucket") 
+				{ 
+					$description = $property.Value
+				} 
+			} 
+		}
+
+		$scriptr.Options.FileName = $filepath + "\" + $($description) + "\" + $($dbObj.Name) + "_ForeignKey" + ".sql"
+		$scriptr.Script($dbObj)
+	}
 }
 
 task PackageForDev {
